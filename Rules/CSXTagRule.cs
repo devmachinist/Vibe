935b7c5408520f80addd430947a92a90e53e3253
Devmachinist <x@devmachinist.com>
2025-01-28 06:27:05 -0600

    reduction

--- Rules/CSXTagRule.cs
+++ Rules/CSXTagRule.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.CodeAnalysis.CSharp.Scripting;
 using AngleSharp.Html.Dom.Events;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Reflection;
