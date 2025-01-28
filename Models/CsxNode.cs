018b048cfbc5ba8ad05a91f261d0677c3c3429b6
Devmachinist <x@devmachinist.com>
2025-01-28 01:55:02 -0600

    reduced

--- Models/CsxNode.cs
+++ Models/CsxNode.cs
@@ -607,13 +607,7 @@ class {Node.Name.ToUpper()} extends CsxNode {{
         }
         public CsxNode Clone()
         {
-            using (MemoryStream stream = new MemoryStream())
-            {
-                var formatter = new BinaryFormatter();
-                formatter.Serialize(stream, this);
-                stream.Seek(0, SeekOrigin.Begin);
-                return formatter.Deserialize<CsxNode>(stream);
-            }
+            return new CsxNode(this);
         }
 
         public void on(string eventName, object listener)
